c340fa7863ae5e62b635fc1d21064336e8ae257f
stella785 <stellac35@nycstudents.net>
2025-10-08 14:01:47 -0400

    ...

--- dom.js
+++ dom.js
@@ -1,22 +1,153 @@
-const DOMSelectors = {
-  title: document.getElementById("title"),
-  artist: document.getElementById("artist"),
-  url: document.getElementById("url"),
-  display: document.getElementById("display"),
-};
+const sneakers = [
+  {
+    name: "Nike Dunk Lows",
+    price: 120,
+    brand: "Nike",
+    img: "images/Nike dunk.avif",
+    inStock: true,
+  },
+  {
+    name: "Sabrina 3 'Silencer'",
+    price: $100,
+    brand: "Nike",
+    img: "images/sabrina silencer.avif",
+    inStock: true,
+  },
+  {
+    name: "Nike Air Force 1 LE",
+    price: 90,
+    brand: "Nike",
+    img: "images/air force.avif",
+    inStock: true,
+  },
+  {
+    name: "Nike Field General 'Ref'",
+    price: 85,
+    brand: "Nike",
+    img: "images/nike field.avif",
+    inStock: true,
+  },
+  {
+    name: "GEL-CUMULUS 27 GRADE SCHOOL",
+    price: 90,
+    brand: "ASICS",
+    img: "images/gel asic.jpg",
+    inStock: true,
+  },
+  {
+    name: "GEL-RESOLUTION X GRADE SCHOOL",
+    price: 95,
+    brand: "ASICS",
+    img: "images/gel resolution.webp",
+    inStock: true,
+  },
+  {
+    name: "GEL-KAYANO 32 TOKYO",
+    price: 165,
+    brand: "ASICS",
+    img: "images/kayano.webp",
+    inStock: true,
+  },
+  {
+    name: "SNAPDOWN 4 GRADE SCHOOL",
+    price: 65,
+    brand: "ASICS",
+    img: "images/snapdown.webp",
+    inStock: true,
+  },
+  {
+    name: "MATFLEX 7 GRADE SCHOOL",
+    price: 60,
+    brand: "ASIC",
+    img: "images/matflex.webp",
+    inStock: true,
+  },
+  {
+    name: "Nike JAM",
+    price: 120,
+    brand:"Nike",
+    img: "images/jam.avif"
+    inStock: true,
+  },
+  {
+    name: 
+    price:
+    brand:
+    img:
+    inStock: true,
+  },
+  {
+    name:
+    price:
+    brand:
+    img:
+    inStock: true,
+  },
+  {
+    name:
+    price:
+    brand:
+    img:
+    inStock: true,
+  },
+  {
+    name:
+    price:
+    brand:
+    img:
+    inStock: true,
+  },
+  {
+    name:
+    price:
+    brand:
+    img:
+    inStock: true,
+  },
+  {
+    name:
+    price:
+    brand:
+    img:
+    inStock: true,
+  },
+  {
+    name:
+    price:
+    brand:
+    img:
+    inStock: true,
+  },
+  {
+    name:
+    price:
+    brand:
+    img:
+    inStock: true,
+  },
+  {
+    name:
+    price:
+    brand:
+    img:
+    inStock: true,
+  },
+  {
+    name:
+    price:
+    brand:
+    img:
+    inStock: true,
+  }
+]
 
-function inject(album) {
-  DOMSelectors.display.insertAdjacentHTML(
-    "afterbegin",
-    `<div class="display-card">
-      <img class="display-img" src="${album.url}"/>
-      <h2 class="display-artist">${album.artist}</h2>
-      <h3 class="display-album">${album.title}</h3>
-      <button class="remove btn">Remove Album</button>
-    </div>`
-  );
-}
+//created array
+//find the container using query selector
+//create function to add 1 card to screen
 
-function removeAlbum(event) {
-  event.target.parentElement.remove();
+function inject(sneakers) {
+  //put card on screen
+  //insertadjacenthtml
+  //card html string
+  //find in the dom md
 }
